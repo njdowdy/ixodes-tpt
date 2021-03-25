2dc46fc5aaa43b0212e41fe8bb7fe99016945e99
Teresa Mayfield-Meyer <jegelewicz66@gmail.com>
2021-03-25 13:31:56 -0600

    fixes and cleaning

--- deduplicate_names.r
+++ deduplicate_names.r
@@ -30,7 +30,7 @@ higher_taxa$canonical[i] <- ifelse(!is.na(higher_taxa$subgenus[i]), paste(higher
 flag <- c('review')
 review_canonical <- higher_taxa[(higher_taxa$canonical %in% flag), ]
 write.csv(review_canonical,"~/GitHub/ixodes-tpt/output/review_canonical.csv", row.names = FALSE) # these need review
-higher_taxa <- higher_taxa[(higher_taxa$canonical %!in% flag), ]
+higher_taxa <- higher_taxa[(higher_taxa$canonical %!in% flag), ] # extract review items from higher_taxa
 
 # read in cleaned review file
 review_canonical <- read.csv("~/GitHub/ixodes-tpt/input/review_canonical.csv", encoding="UTF-8")
@@ -41,34 +41,34 @@ df <- rbind(higher_taxa, df)
 
 
 # extract blank canonical names for review
-df_review <- df[which(lapply(df$canonical, nchar) == 0), ]
-df <- df[which(lapply(df$canonical, nchar) != 0),] # remove blank canonicals from working file
+# df_review <- df[which(lapply(df$canonical, nchar) == 0), ]
+# df <- df[which(lapply(df$canonical, nchar) != 0),] # remove blank canonicals from working file
 
 # flag duplicate canonical names
 df$dup <- c(duplicated(df$canonical, fromLast = TRUE)  | duplicated(df$canonical))
 
 # extract duplicates from less-favored sources
 # ITIS
-duplicates <- df[which(grepl('^ITIS', df$TPTID) == TRUE &
-                   df$dup == TRUE), ] # ITIS canonical names that are duplicated in other sources
-ITIS_dupes <- nrow(duplicates)
-duplicates_not <- df[which(grepl('^ITIS', df$TPTID) == TRUE &
-                 df$dup == FALSE), ] # ITIS canonical names that are not duplicated in other sources
-df <- df[which(grepl('^ITIS', df$TPTID) == FALSE), ] # remove all ITIS rows
-df <- rbind(df, duplicates_not) # return ITIS rowas that are not dupes to working file
+# duplicates <- df[which(grepl('^ITIS', df$TPTID) == TRUE &
+#                    df$dup == TRUE), ] # ITIS canonical names that are duplicated in other sources
+# ITIS_dupes <- nrow(duplicates)
+# duplicates_not <- df[which(grepl('^ITIS', df$TPTID) == TRUE &
+#                  df$dup == FALSE), ] # ITIS canonical names that are not duplicated in other sources
+# df <- df[which(grepl('^ITIS', df$TPTID) == FALSE), ] # remove all ITIS rows
+# df <- rbind(df, duplicates_not) # return ITIS rowas that are not dupes to working file
 
 # Missing data check ITIS
-if(TOTAL != nrow(df) + 
-   nrow(duplicates)) {print('Some records appear to have been lost. Script was terminated. Please address errors.')
-} else {
-  print('all records accounted for')
-} # End of Missing data check ITIS
+# if(TOTAL != nrow(df) + 
+#    nrow(duplicates)) {print('Some records appear to have been lost. Script was terminated. Please address errors.')
+# } else {
+#   print('all records accounted for')
+# } # End of Missing data check ITIS
 
 # FMNH
-duplicate_append <- df[which(grepl('^FMNH', df$TPTID) == TRUE &
+duplicates <- df[which(grepl('^FMNH', df$TPTID) == TRUE &
                          df$dup == TRUE), ] # FMNH canonical names that are duplicated in other sources
-FMNH_dupes <- nrow(duplicate_append)
-duplicates <- rbind(duplicates, duplicate_append)
+FMNH_dupes <- nrow(duplicates)
+# duplicates <- rbind(duplicates, duplicate_append)
 duplicates_not <- df[which(grepl('^FMNH', df$TPTID) == TRUE &
                              df$dup == FALSE), ] # FMNH canonical names that are not duplicated in other sources
 df <- df[which(grepl('^FMNH', df$TPTID) == FALSE), ] # remove all FMNH rows
@@ -99,42 +99,42 @@ if(TOTAL != nrow(df) +
 } # End of Missing data check NMNH
 
 # GBIF
-duplicate_append <- df[which(grepl('^GBIF', df$TPTID) == TRUE &
-                               df$dup == TRUE), ] # GBIF canonical names that are duplicated in other sources
-GBIF_dupes <- nrow(duplicate_append)
-duplicates <- rbind(duplicates, duplicate_append)
-duplicates_not <- df[which(grepl('^GBIF', df$TPTID) == TRUE &
-                                    df$dup == FALSE), ] # GBIF canonical names that are not duplicated in other sources
-df <- df[which(grepl('^GBIF', df$TPTID) == FALSE), ] # remove all GBIF rows
-df <- rbind(df, duplicates_not) # return GBIF rows that are not dupes to working file
-
-# Missing data check GBIF
-if(TOTAL != nrow(df) + 
-   nrow(duplicates)) {print('Some records appear to have been lost. Script was terminated. Please address errors.')
-} else {
-  print('all records accounted for')
-} # End of Missing data check GBIF
+# duplicate_append <- df[which(grepl('^GBIF', df$TPTID) == TRUE &
+#                                df$dup == TRUE), ] # GBIF canonical names that are duplicated in other sources
+# GBIF_dupes <- nrow(duplicate_append)
+# duplicates <- rbind(duplicates, duplicate_append)
+# duplicates_not <- df[which(grepl('^GBIF', df$TPTID) == TRUE &
+#                                     df$dup == FALSE), ] # GBIF canonical names that are not duplicated in other sources
+# df <- df[which(grepl('^GBIF', df$TPTID) == FALSE), ] # remove all GBIF rows
+# df <- rbind(df, duplicates_not) # return GBIF rows that are not dupes to working file
+# 
+# # Missing data check GBIF
+# if(TOTAL != nrow(df) + 
+#    nrow(duplicates)) {print('Some records appear to have been lost. Script was terminated. Please address errors.')
+# } else {
+#   print('all records accounted for')
+# } # End of Missing data check GBIF
 
 # BYU
-duplicate_append <- df[which(grepl('^BYU', df$TPTID) == TRUE &
-                               df$dup == TRUE), ] # BYU canonical names that are duplicated in other sources
+# duplicate_append <- df[which(grepl('^BYU', df$TPTID) == TRUE &
+#                                df$dup == TRUE), ] # BYU canonical names that are duplicated in other sources
+# BYU_dupes <- nrow(duplicate_append)
+# duplicates <- rbind(duplicates, duplicate_append)
+# duplicates_not <- df[which(grepl('^BYU', df$TPTID) == TRUE &
+#                                     df$dup == FALSE), ] # BYU canonical names that are not duplicated in other sources
+# df <- df[which(grepl('^BYU', df$TPTID) == FALSE), ] # remove all BYU rows
+# df <- rbind(df, duplicates_not) # return BYU rows that are not dupes to working file
+# 
+# # Missing data check BYU
+# if(TOTAL != nrow(df) + 
+#    nrow(duplicates)) {print('Some records appear to have been lost. Script was terminated. Please address errors.')
+# } else {
+#   print('all records accounted for')
+# } # End of Missing data check BYU
+
+# BYU
+duplicate_append <- df[which(duplicated(df$canonical)),] # BYU canonical names that are duplicated in other sources
 BYU_dupes <- nrow(duplicate_append)
-duplicates <- rbind(duplicates, duplicate_append)
-duplicates_not <- df[which(grepl('^BYU', df$TPTID) == TRUE &
-                                    df$dup == FALSE), ] # BYU canonical names that are not duplicated in other sources
-df <- df[which(grepl('^BYU', df$TPTID) == FALSE), ] # remove all BYU rows
-df <- rbind(df, duplicates_not) # return BYU rows that are not dupes to working file
-
-# Missing data check BYU
-if(TOTAL != nrow(df) + 
-   nrow(duplicates)) {print('Some records appear to have been lost. Script was terminated. Please address errors.')
-} else {
-  print('all records accounted for')
-} # End of Missing data check BYU
-
-# CoL
-duplicate_append <- df[which(duplicated(df$canonical)),] # CoL canonical names that are duplicated in other sources
-CoL_dupes <- nrow(duplicate_append)
 duplicates <- rbind(duplicates, duplicate_append)
 df <- df[which(!duplicated(df$canonical)),] # deduplicated list
 
@@ -147,23 +147,26 @@ if(TOTAL != nrow(df) +
 
 # add duplicates and uniques to source summary dataframe
 BYU <- nrow(df[grep("BYU*", df$TPTID), ])
-COL <- nrow(df[grep("CoL*", df$TPTID), ]) # number of deduplicated records in CoL Source
+# COL <- nrow(df[grep("CoL*", df$TPTID), ]) # number of deduplicated records in CoL Source
 FMNH <- nrow(df[grep("FMNH*", df$TPTID), ]) # number of deduplicated records in FMNH Source
-GBIF <- nrow(df[grep("GBIF*", df$TPTID), ]) # number of deduplicated records in GBIF Source
-ITIS <- nrow(df[grep("ITIS*", df$TPTID), ]) # number of deduplicated records in ITIS Source
+# GBIF <- nrow(df[grep("GBIF*", df$TPTID), ]) # number of deduplicated records in GBIF Source
+# ITIS <- nrow(df[grep("ITIS*", df$TPTID), ]) # number of deduplicated records in ITIS Source
 NMNH <- nrow(df[grep("NMNH*", df$TPTID), ]) # number of deduplicated records in NMNH Source
-TOTAL <- BYU + COL + FMNH + GBIF + ITIS + NMNH # number of deduplicated records in all sources
-TOTAL_Dupes <- BYU_dupes + CoL_dupes + FMNH_dupes + GBIF_dupes + ITIS_dupes + NMNH_dupes # number of duplicated records in all sources
-source_summary$duplicated_names <- c(BYU_dupes, CoL_dupes, FMNH_dupes, GBIF_dupes, ITIS_dupes, NMNH_dupes, TOTAL_Dupes)
-source_summary$unique_names <- c(BYU, COL, FMNH, GBIF, ITIS, NMNH, TOTAL)
+TOTAL <- BYU + FMNH + NMNH # number of deduplicated records in all sources
+TOTAL_Dupes <- BYU_dupes + FMNH_dupes + NMNH_dupes # number of duplicated records in all sources
+source_summary$duplicated_names <- c(BYU_dupes, FMNH_dupes, NMNH_dupes, TOTAL_Dupes)
+source_summary$unique_names <- c(BYU, FMNH, NMNH, TOTAL)
 
 
 # separate sources
-BYU_Siphonaptera <- df[which(grepl('Byu*', df$TPTID) == TRUE), ]
-CoL_Siphonaptera <- df[which(grepl('Col*', df$TPTID) == TRUE), ]
-FMNH_Siphonaptera <- df[which(grepl('Fmnh*', df$TPTID) == TRUE), ]
-GBIF_Siphonaptera <- df[which(grepl('Gbif*', df$TPTID) == TRUE), ]
-ITIS_Siphonaptera <- df[which(grepl('Itis*', df$TPTID) == TRUE), ]
-NMNH_Siphonaptera <- df[which(grepl('Nmnh*', df$TPTID) == TRUE), ]
+# BYU_Siphonaptera <- df[which(grepl('Byu*', df$TPTID) == TRUE), ]
+# CoL_Siphonaptera <- df[which(grepl('Col*', df$TPTID) == TRUE), ]
+# FMNH_Siphonaptera <- df[which(grepl('Fmnh*', df$TPTID) == TRUE), ]
+# GBIF_Siphonaptera <- df[which(grepl('GBIF*', df$TPTID) == TRUE), ]
+# ITIS_Siphonaptera <- df[which(grepl('Itis*', df$TPTID) == TRUE), ]
+# NMNH_Siphonaptera <- df[which(grepl('Nmnh*', df$TPTID) == TRUE), ]
 
 df <- rbind(BYU_Siphonaptera, CoL_Siphonaptera, FMNH_Siphonaptera, GBIF_Siphonaptera, ITIS_Siphonaptera, NMNH_Siphonaptera)
+
+write.csv(df,"~/GitHub/ixodes-tpt/output/TPT_Siphonaptera.csv", row.names = FALSE) # write out final list
+write.csv(duplicates,"~/GitHub/ixodes-tpt/output/TPT_Siphonaptera_duplicates.csv", row.names = FALSE)
